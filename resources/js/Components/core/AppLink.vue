5729a74943c096c27fcb6a39625702bcb968c58f
GonzaloGPF <zalo.pabon@gmail.com>
2024-02-17 11:25:21 +0100

    Styles changes

--- resources/js/Components/core/AppLink.vue
+++ resources/js/Components/core/AppLink.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3'
 
 defineProps({
     label: {
@@ -20,7 +20,7 @@ defineProps({
   <Link
     :href="href"
     :title="title || label"
-    class="font-semibold text-gray-600 hover:text-gray-300 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-500"
+    class="font-semibold text-gray-600 hover:text-gray-300 dark:text-gray-400 dark:hover:text-white"
   >
     <slot>{{ label }}</slot>
   </Link>
